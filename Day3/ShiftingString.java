87054dd4eed0bed8c883a1b445d00805d661ec75
Aysha-shamli <ayshashamli1999@gmail.com>
2023-11-03 12:55:03 +0530

    added some new programs

--- Day3/ShiftingString.java
+++ Day3/ShiftingString.java
@@ -1,13 +1,17 @@
 package com.Day3;
 
 import java.util.Scanner;
-
 public class ShiftingString {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the sentence: ");
-        int x = scanner.nextInt();
-
+        String input = scanner.nextLine();
+        String word = "";
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            char newChar = (char)(currentChar + 1);
+            word = word + newChar;
+        }
+        System.out.println("Manipulated string " + word);
     }
-
 }
